d62e41a837dd48adda7b597da88927e0f9de419b
Arthur Ariza <37642667+a3vezes@users.noreply.github.com>
2021-01-15 09:11:16 -0300

    Update api.js

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // ip unimed 192.168.137.26
-  // ip casa 192.168.15.67
+
   baseURL: 'http://192.168.15.67:5000/api/v1/',
   timeout: 5000,
 });
